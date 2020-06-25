244afdacb57e0e33f1e232213262102a72ffe201
Robert Roose <robert@shivercube.com>
2020-06-25 18:04:33 +0100

    Wait indefinitely

--- files/port-forwarding.sh
+++ files/port-forwarding.sh
@@ -10,6 +10,6 @@ if [ "$MODE" = paper ]; then
 fi
 
 echo "Waiting for API to start"
-wait-for-it 127.0.0.1:$PORT
+wait-for-it -t 0 127.0.0.1:$PORT
 echo "API is ready"
 socat TCP-LISTEN:7000,fork TCP:127.0.0.1:$PORT
